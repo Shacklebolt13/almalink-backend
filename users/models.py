fedfccb33a122092ad98cb7b822fc39f9eb7e06c
Shacklebolt13 <19cse223.gagandeepsinghbhambrah@giet.edu>
2022-11-30 18:26:34 +0530

    added nullable username

--- users/models.py
+++ users/models.py
@@ -5,3 +5,5 @@ from django_use_email_as_username.models import BaseUser, BaseUserManager
 class User(BaseUser):
     skill = models.CharField(max_length=100, blank=True, null=True)
     objects = BaseUserManager()
+
+    username = models.CharField(max_length=100, blank=True, null=True)
